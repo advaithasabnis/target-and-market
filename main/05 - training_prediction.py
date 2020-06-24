5f77cfb34d3de2c35f1b9d8f0d0a3bf3dc441f16
Advait <advait.iitb@gmail.com>
2020-06-23 21:06:05 -0400

    update: remove feature scaling

--- main/05 - training_prediction.py
+++ main/05 - training_prediction.py
@@ -8,13 +8,12 @@ Train and predict probability, for each user, that they are like a paid user usi
 Output probability predictions for front-end dashboard.
 """
 
-import numpy as np
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc, make_scorer
 from sklearn.model_selection import GridSearchCV
+
 from targetandmarket.config import data_folder, appData_folder
 
 #%% Custom scoring function
@@ -40,21 +39,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     random_state=0,
                                                     stratify=y)
 
-#%% Preprocessing
-preprocessor = StandardScaler()
-X_train = preprocessor.fit_transform(X_train)
-X_test = preprocessor.transform(X_test)
-
-
-#%% Tuning Xgboost
+#%% Final tuning of Xgboost
 # Note: parameters were narrowed down from a wider range
 # and the below params only show the last round of tuning
-params = {'n_estimators': [80, 90, 100],
-          'max_depth': [3, 4],
-          'subsample': [0.7, 0.8, 0.9],
-          'learning_rate': [0.065, 0.07, 0.075],
-          'colsample_bytree': [0.6, 0.7],
-          'reg_lambda': [16, 32],
+params = {'n_estimators': [90, 100],
+          'max_depth': [4],
+          'subsample': [0.65, 0.7, 0.75],
+          'learning_rate': [0.065, 0.07],
+          'colsample_bytree': [0.6, 0.7, 0.8],
+          'reg_lambda': [16],
           'gamma': [1],
           'min_child_weight': [1.5],
           'objective': ['binary:logistic']
@@ -73,18 +66,14 @@ y_score_test = grid.predict_proba(X_test)[:, 1:]
 print(f'Test ROC AUC: {auprc(y_test, y_score_test):0.3f}')
 
 #%% Training model on full dataset
-xgb_classifier = xgb.XGBClassifier(n_estimators=100, max_depth=4, learning_rate=0.065,
-                                   colsample_bytree=0.7, subsample=0.8, reg_lambda=16, gamma=1,
+xgb_classifier = xgb.XGBClassifier(n_estimators=90, max_depth=4, learning_rate=0.07,
+                                   colsample_bytree=0.7, subsample=0.7, reg_lambda=16, gamma=1,
                                    min_child_weight=1.5, objective='binary:logistic')
 
-X_full = preprocessor.fit_transform(X)
-y_full = y.copy()
-X_full = pd.DataFrame(data=X_full, columns=num_features)
-
-xgb_classifier.fit(X_full, y_full)
-y_score = xgb_classifier.predict_proba(X_full)[:, 1:]
-print(f'ROC AUC: {roc_auc_score(y_full, y_score):0.3f}')
-print(f'AUPRC: {auprc(y_full, y_score):0.3f}')
+xgb_classifier.fit(X, y)
+y_score = xgb_classifier.predict_proba(X)[:, 1:]
+print(f'ROC AUC: {roc_auc_score(y, y_score):0.3f}')
+print(f'AUPRC: {auprc(y, y_score):0.3f}')
 
 #%% Save predictions for Dash app
 user_predictions = user_data.copy()
